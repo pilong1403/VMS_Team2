c40d0c350d9b75db0a6536fc9419545102f5f12f
Tcanh12 <canhnvthe186121@fpt.edu.vn>
2025-09-26 22:50:18 +0700

    update JPA entities to match DB schema
    - use lowercase table names with schema "dbo"
    - remove TrustScoreLog entity & trust_score fields

--- src/main/java/com/fptuni/vms/model/Feedback.java
+++ src/main/java/com/fptuni/vms/model/Feedback.java
@@ -2,7 +2,7 @@ package com.fptuni.vms.model;
 
 import jakarta.persistence.*;
 import java.time.LocalDateTime;
-
+//canh ne
 @Entity
 @Table(name = "feedback" , schema = "dbo")
 public class Feedback {
