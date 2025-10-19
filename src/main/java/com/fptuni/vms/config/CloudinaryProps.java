1aa2691cff30f7bfc7fd859a5284cf8bb463efe6
Nguyen Phi Long <nlong1403@gmail.com>
2025-10-19 13:49:21 +0700

    motivation

--- src/main/java/com/fptuni/vms/config/CloudinaryProps.java
+++ src/main/java/com/fptuni/vms/config/CloudinaryProps.java
@@ -1,5 +1,6 @@
 package com.fptuni.vms.config;
 
+///dssdfs
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
@@ -10,14 +11,34 @@ import jakarta.validation.constraints.NotBlank;
 @ConfigurationProperties(prefix = "cloudinary")
 @Validated
 public class CloudinaryProps {
-    @NotBlank private String cloudName;
-    @NotBlank private String apiKey;
-    @NotBlank private String apiSecret;
+    @NotBlank
+    private String cloudName;
+    @NotBlank
+    private String apiKey;
+    @NotBlank
+    private String apiSecret;
 
-    public String getCloudName() { return cloudName; }
-    public void setCloudName(String cloudName) { this.cloudName = cloudName; }
-    public String getApiKey() { return apiKey; }
-    public void setApiKey(String apiKey) { this.apiKey = apiKey; }
-    public String getApiSecret() { return apiSecret; }
-    public void setApiSecret(String apiSecret) { this.apiSecret = apiSecret; }
+    public String getCloudName() {
+        return cloudName;
+    }
+
+    public void setCloudName(String cloudName) {
+        this.cloudName = cloudName;
+    }
+
+    public String getApiKey() {
+        return apiKey;
+    }
+
+    public void setApiKey(String apiKey) {
+        this.apiKey = apiKey;
+    }
+
+    public String getApiSecret() {
+        return apiSecret;
+    }
+
+    public void setApiSecret(String apiSecret) {
+        this.apiSecret = apiSecret;
+    }
 }
